836814c88a43e561764b3ebc0945ca04e89126e7
pavel <ironerg@gmail.com>
2022-09-04 20:43:38 +0300

    add message helper

--- config/app.php
+++ config/app.php
@@ -1,7 +1,12 @@
 <?php
 
+session_start();
+
 // Require global constants
 require_once __DIR__ . '/constants.php';
+
+// Require helper functions
+require_once __DIR__ . '/helpers.php';
 
 // Require composer autoloader
 require_once ROOT . '/vendor/autoload.php';
